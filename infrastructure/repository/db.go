ee0a8905507d399785ba514b3cbd189cdcbb789a
Ad3bay0c <aclintonadebayo@gmail.com>
2022-01-15 22:54:09 +0100

    Db access methods

--- infrastructure/repository/db.go
+++ infrastructure/repository/db.go
@@ -1,5 +1,17 @@
 package repository
 
+import (
+	"gorm.io/gorm"
+)
+
 type DB struct {
-	Db
+	Db	*gorm.DB
+}
+
+func (db *DB) NewPostgresDB() {
+	//database, err := gorm.Open(postgres.Open(""), &gorm.Config{})
+	//if err != nil {
+	//	panic(err)
+	//}
+	//db.Db = database
 }
