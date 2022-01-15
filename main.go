f86cdfb9f4d596d8caab4fc77c68d1f212da6f10
Ad3bay0c <aclintonadebayo@gmail.com>
2022-01-15 22:55:49 +0100

    create main file and initialized server struct

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
-package ddd_structure
+package main
+
+import (
+	"github.com/Ad3bay0c/ddd-structure/application/handler"
+	"github.com/Ad3bay0c/ddd-structure/application/server"
+	"github.com/Ad3bay0c/ddd-structure/infrastructure/repository"
+)
+
+func main() {
+	db := &repository.DB{}
+	db.NewPostgresDB()
+	app := &handler.Application{
+		UserDb: db,
+	}
+	s := server.NewServer(app)
+
+	s.Start()
+}
