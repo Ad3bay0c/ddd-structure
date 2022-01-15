4908aaf112b03878219158e8d411d13d8d166bc1
Ad3bay0c <aclintonadebayo@gmail.com>
2022-01-15 22:52:35 +0100

    Set up the routers

--- application/server/router.go
+++ application/server/router.go
@@ -1,1 +1,58 @@
 package server
+
+import (
+	"fmt"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"log"
+	"os"
+	"time"
+)
+
+// defineRouter defines the routes for the server
+func (s *server) defineRoutes(r *gin.Engine) {
+	r.GET("/", s.App.Create())
+}
+
+// SetupRouter set up the router for the server
+func (s *server) SetupRouter() *gin.Engine {
+	ginMode := os.Getenv("GIN_MODE")
+	if ginMode == "test" {
+		log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+		r := gin.New()
+		s.defineRoutes(r)
+		return r
+	}
+
+	r := gin.New()
+
+	r.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		// your custom format
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+			param.ClientIP,
+			param.TimeStamp.Format(time.RFC1123),
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	}))
+	r.Use(gin.Recovery())
+	// setup cors
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"POST", "GET", "PUT", "PATCH", "DELETE"},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		// AllowOriginFunc: func(origin string) bool {
+		// 	return origin == "https://github.com"
+		// },
+		MaxAge: 12 * time.Hour,
+	}))
+	s.defineRoutes(r)
+	return r
+}
