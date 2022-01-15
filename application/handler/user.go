ac42ff5733c3afa26686a017e5f2c9c4021cd904
Ad3bay0c <aclintonadebayo@gmail.com>
2022-01-15 22:52:07 +0100

    Added Create method to application struct to implement DBInterface

--- application/handler/user.go
+++ application/handler/user.go
@@ -1,1 +1,11 @@
 package handler
+
+import "github.com/gin-gonic/gin"
+
+func (app *Application) Create() gin.HandlerFunc {
+	return func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "message": "pong",
+        })
+    }
+}
